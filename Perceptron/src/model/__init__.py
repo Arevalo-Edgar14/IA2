c0a8c4e01b2fabced9267eb4fb8ef8925259b09f
Erev14 <erevjoach14@gmail.com>
2021-08-30 13:14:04 -0500

    Feature: Add model need functions definition

--- Perceptron/src/model/__init__.py
+++ Perceptron/src/model/__init__.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Perceptron:
     LEARNING_RATE = 0.001
     MAX_EPOCH = 100
@@ -7,21 +10,32 @@ class Perceptron:
         self.y_ = []
         self.learning_rate = self.LEARNING_RATE
         self.max_epoch = self.MAX_EPOCH
+        self.converged_epoch = -1
+        self.confusion_matrix = []
 
     ########  1.- Rúbrica Canvas para espacio R^2(Plano cartesiano) ########
     # 10 Pts
     # TODO receive dictionary with x,y coordinates and value 1 o 0 (1 o -1 if
     #  bipolar labels) and save it in the testing data
+    def set_xy(self, xy):
+        pass
 
     ######## 2.- Rúbrica Hiperparámetros: learning rate , épocas máximas
     # ########
     # 10 Pts / 5 si no se cumple alguno
     # TODO receive new value for learning rate
     # TODO receive new value for max epoch
+    def set_learning_rate(self, value):
+        pass
+
+    def set_max_epoch(self, value):
+        pass
 
     ######## 3.- Rúbrica Inicialización de pesos ########
     # 10 Pts
     # TODO init xy size weights with random values
+    def init(self, event):
+        pass
 
     ######## 4.- Rúbrica Algoritmo de entrenamiento y linea en cada actualización
     ########
@@ -29,6 +43,11 @@ class Perceptron:
     # Una vez inicializado
     # TODO training function that call the training_epoch function
     # TODO training_epoch function
+    def epoch(self):
+        pass
+
+    def train(self):
+        pass
 
     ######## 5.- Rúbrica Convergencia final ########
     # 20 Pts
@@ -38,4 +57,5 @@ class Perceptron:
     # TODO have a confusion matrix and set it at the end of the training
     #  function
     # TODO recive a dictionary with coordinates to test
-
+    def test(self, value):
+        pass
