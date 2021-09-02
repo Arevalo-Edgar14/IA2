94533aefa625c32d3dfcc1f0e6c7393d0b2fe871
Erev14 <erevjoach14@gmail.com>
2021-09-02 07:56:42 -0500

    Feature: move data initialize to a method to call from constructor and from reset method Add implementation for set point, coordinate, xy Add set learning rate, max epoch, bipolar, bounds
    Add init implementation, set x y as np arrays and change the button value (3 for right click) to bipolar or 0 if bipolar, create uniform randoms between bounds, and draw the line
    
    Add calculate point to draw line, in base of the straight formula
    
    Add implementation of activation
    
    Add implementation of pw
    
    Add error calculation
    
    Add epoch process
    
    Add training

--- Perceptron/src/model/__init__.py
+++ Perceptron/src/model/__init__.py
@@ -1,61 +1,145 @@
+import time
+
 import numpy as np
+from pubsub import pub
 
 
 class Perceptron:
-    LEARNING_RATE = 0.001
-    MAX_EPOCH = 100
+    # LEARNING_RATE = 0.001
+    LEARNING_RATE = 0.5
+    MAX_EPOCH = 50
+    BOUND = 1
 
     def __init__(self):
+        self.reset("init")
+
+    def reset(self, event):
         self.x_ = []
         self.y_ = []
         self.learning_rate = self.LEARNING_RATE
         self.max_epoch = self.MAX_EPOCH
         self.converged_epoch = -1
         self.confusion_matrix = []
+        self.bipolar = False
+        self.bounds = [-self.BOUND, self.BOUND]
+        self.is_fitted = False
+        self.w = []
+        self.x = []
+        self.y = []
 
-    ########  1.- Rúbrica Canvas para espacio R^2(Plano cartesiano) ########
-    # 10 Pts
-    # TODO receive dictionary with x,y coordinates and value 1 o 0 (1 o -1 if
-    #  bipolar labels) and save it in the testing data
     def set_xy(self, xy):
-        pass
+        print(f'Setting xy: {xy} on perceptron')
+        self.x_.append(xy["pos"])
+        self.y_.append(xy["value"])
+        # send to add the point on the canvas no need to take care of bipolar
+        # because on the view do not care
+        pub.sendMessage("point_added", point={"x": xy["pos"][0],
+                                              "y": xy["pos"][1],
+                                              "value":
+                                                  0 if xy["value"] == 3 else 1})
 
-    ######## 2.- Rúbrica Hiperparámetros: learning rate , épocas máximas
-    # ########
-    # 10 Pts / 5 si no se cumple alguno
-    # TODO receive new value for learning rate
-    # TODO receive new value for max epoch
-    def set_learning_rate(self, value):
-        pass
+    def set_learning_rate(self, learning_rate):
+        print(f'Setting learning_rate: {learning_rate} on perceptron')
+        self.learning_rate = learning_rate
 
-    def set_max_epoch(self, value):
-        pass
+    def set_max_epoch(self, max_epoch):
+        print(f'Setting max_epoch: {max_epoch} on perceptron')
+        self.max_epoch = max_epoch
 
-    ######## 3.- Rúbrica Inicialización de pesos ########
-    # 10 Pts
-    # TODO init xy size weights with random values
+    def set_bipolar(self, bipolar):
+        print(f'Setting bipolar: {bipolar} on perceptron')
+        self.bipolar = bipolar
+
+    def set_bounds(self, bounds):
+        print(f'Setting bounds: {bounds} on perceptron')
+        # bounds come in negative values
+        self.bounds = [bounds, -bounds]
+
     def init(self, event):
-        pass
+        print(f'Initializing Perceptron Weights')
+        self.x = np.array(self.x_)
+        self.y = np.array(self.y_)
+        # -3 is the right click value, set corresponding valued as -1 if is
+        # bipolar or 0 if not
+        self.y[self.y == 3] = -1 if self.bipolar else 0
+        # rand values from -bound to bound of x size for this example 2 + 1 (
+        # +1 because W0)
+        # x.shape(points number, coords) that's why we use x.shape[1] to just
+        # set coords elements number as weights, example x, y, just 2 weights
+        self.w = np.random.uniform(self.bounds[0], self.bounds[1],
+                                   self.x.shape[1] + 1)
+        pub.sendMessage('weights_changed', weights=self.w)
+        self.calculate_points_to_draw_line()
 
-    ######## 4.- Rúbrica Algoritmo de entrenamiento y linea en cada actualización
-    ########
-    # 30 Pts
-    # Una vez inicializado
-    # TODO training function that call the training_epoch function
-    # TODO training_epoch function
-    def epoch(self):
-        pass
+    def calculate_points_to_draw_line(self):
+        x1 = np.array([self.bounds[0] - 2, self.bounds[1] + 2])
+        m = -self.w[1] / self.w[2]
+        b = self.w[0] / self.w[2]
+        x2 = m * x1 + b
+        pub.sendMessage('draw_line', data={"x": x1, "y": x2})
 
-    def train(self):
-        pass
+    def activation(self, xw):
+        if xw >= 0:
+            return 1
+        return -1 if self.bipolar else 0
 
-    ######## 5.- Rúbrica Convergencia final ########
-    # 20 Pts
-    # Al finalizar el entrenamiento
-    # TODO have a epoch converged variable and set it on the iterations when
-    #  the error = 0
-    # TODO have a confusion matrix and set it at the end of the training
-    #  function
-    # TODO recive a dictionary with coordinates to test
-    def test(self, value):
-        pass
+    # test method
+    def Pw(self, x):
+        # x dot product weights
+        xw = np.dot(x, self.w)
+        # return self.activation(xw)
+        return 1 if xw >= 0 else 0
+
+    def error(self, x, i):
+        return self.y[i] - self.Pw(x)
+
+    def epoch(self, current_epoch):
+        print(f'new epoch {current_epoch}')
+        pub.sendMessage('current_epoch_change', epoch=current_epoch)
+        done = True
+        errors = 0
+        for i, xi in enumerate(self.x):
+            # x0 = -1 because w0 = Theta need it as input is place into the
+            # for to avoid x.size > y.size
+            xi = np.insert(xi, 0, -1.0)
+
+            error = self.error(xi, i)
+            # errors += abs(error)
+            if error != 0:
+                errors += 1
+                done = False
+                self.w = self.w + np.multiply((self.learning_rate * error), xi)
+                pub.sendMessage('weights_changed', weights=self.w)
+                self.calculate_points_to_draw_line()
+
+        pub.sendMessage('errors',
+                        data={"errors": errors,
+                              "epoch": current_epoch})
+        return done
+
+    def train(self, event):
+        print(f'Start Training on perceptron')
+        done = False
+        current_epoch = 0
+        while not done and current_epoch < self.max_epoch:
+            current_epoch += 1
+            done = self.epoch(current_epoch)
+        self.converged_epoch = current_epoch - 1
+        pub.sendMessage('converge',
+                        converge={"converge": done,
+                                  "converged_epoch": self.converged_epoch})
+        self.is_fitted = True if done else False
+        # converge if done == TRUE else not converge
+
+        ######## 5.- Rúbrica Convergencia final ########
+        # 20 Pts
+        # Al finalizar el entrenamiento
+        # TODO have a confusion matrix and set it at the end of the training
+        #  function
+
+    def test(self, xy):
+        # TO SEE if need
+        # np.insert(test, 0, -1.0)
+        pub.sendMessage("point_added", point={"x": xy["pos"][0],
+                                              "y": xy["pos"][1],
+                                              "value": self.Pw(xy)})
