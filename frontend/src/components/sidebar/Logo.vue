60cf144d6f653fbf8b289bd7d92b0ffc029f4459
Erev14 <erevjoach14@gmail.com>
2022-02-10 09:26:27 +0000

    feature(development): add Logo component

---
+++ frontend/src/components/sidebar/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+  <span :class="image">
+    <img :src="logo" :alt="alt" />
+  </span>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    image: String,
+    logo: String,
+    alt: String,
+  },
+})
+export default class Logo extends Vue {}
+</script>
