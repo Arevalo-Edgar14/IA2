5d87a91c19b01d5834b2f935a4f44011a4ebb977
Erev14 <erevjoach14@gmail.com>
2022-02-16 09:03:27 +0000

    build: add whatch options to active hotreaload build: add public to give access websocket hotreaload resolve to container

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   devServer: {
+    watchOptions: {
+      ignored: /node_modules/,
+      aggregateTimeout: 300,
+      poll: 500
+    },
     // compress: true,
     // host: 'projectname.local'
+    // public: 'ia2_devcontainer-app-1:8080',
+    // public: '127.0.0.11:8080',
+    public: '0.0.0.0:8080',
     disableHostCheck: true,
     port: 4000
   }
