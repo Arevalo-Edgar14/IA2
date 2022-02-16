b9faca478930f264afee11d4de63ceba0f385089
Erev14 <erevjoach14@gmail.com>
2022-02-16 08:52:22 +0000

    build: fix step into folder with direct command

--- start.sh
+++ start.sh
@@ -7,10 +7,8 @@ nginx -e /home/runner/$REPL_SLUG/logs/error.log -c /home/runner/$REPL_SLUG/nginx
 cd backend
 python app.py
 
-cd ../frontend
-yarn install
-yarn run serve
-cd ..
+yarn --cwd frontend/ install
+yarn --cwd frontend/ run serve
 
 #mkdir data
 #jupyter notebook --ip 0.0.0.0 --port 8080 --notebook-dir /home/runner/$REPL_SLUG/data
